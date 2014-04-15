d47571f6530a40b03e29ec0a7be7ba5d16b729ad
Eric Lee <eric.lee@nurun.com>
2014-04-15 17:41:45 +0800

    Add nginx config for efc project

---
+++ modules/lancome-efforless-chic/manifests/init.pp
@@ -0,0 +1,25 @@
+class lancome-efforless-chic {
+
+  service {
+    'nginx':
+      ensure  => 'running',
+      enable  => 'true'
+  }
+
+  file {
+    '/etc/nginx/sites-available/efc':
+      content  => template("lancome-efforless-chic/nginx-www.erb"),
+      owner   => 'root',
+      group   => 'root',
+      notify  => Exec['enable-nginx-site'],
+  }
+
+  exec {
+    'enable-nginx-site':
+      command => 'rm -rf * && ln -s ../sites-available/efc',
+      creates => '/etc/nginx/sites-enabled/efc',
+      cwd     => '/etc/nginx/sites-enabled',
+      user    => 'root',
+      notify  => Service['nginx']
+  }
+}
